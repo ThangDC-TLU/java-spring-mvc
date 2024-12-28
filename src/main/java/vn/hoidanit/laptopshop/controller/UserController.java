66de19a4e6340b2014f8f6dde2cea84cf18409e4
ThangDC-TLU <dinhcaothang08102004@gmail.com>
2024-12-28 13:25:25 +0700

    view engine

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,6 +1,7 @@
 package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,15 +10,7 @@ import vn.hoidanit.laptopshop.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-// @Controller
-// public class UserController {
-//     @RequestMapping("/")
-//     public String getHomePage() {
-//         return "hello controller";
-//     }
-// }
-
-@RestController
+@Controller
 public class UserController {
     private UserService userService;
 
@@ -25,9 +18,27 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("")
-
-    public String getHomePage() {
-        return this.userService.handelHello();
+    @RequestMapping("/")
+    public String getHomePage(Model model) {
+        String test = this.userService.handelHello();
+        model.addAttribute("eric", test);
+        model.addAttribute("hoidanit", "from controller with jsp");
+        return "hello";
     }
 }
+
+// @RestController
+// public class UserController {
+// // Dependency Injection: DI
+// private UserService userService;
+
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping("")
+
+// public String getHomePage() {
+// return this.userService.handelHello();
+// }
+// }
