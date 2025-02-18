927f342ffdb6b770159a89bdba1b61134a1443b0
ThangDC-TLU <dinhcaothang08102004@gmail.com>
2025-02-18 22:39:02 +0700

    su dung data transfer object de tao user

--- src/main/java/vn/hoidanit/laptopshop/controller/client/HomePageController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/HomePageController.java
@@ -2,19 +2,32 @@ package vn.hoidanit.laptopshop.controller.client;
 
 import java.util.List;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import vn.hoidanit.laptopshop.domain.Product;
+import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.domain.dto.RegisterDTO;
 import vn.hoidanit.laptopshop.service.ProductService;
+import vn.hoidanit.laptopshop.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomePageController {
     public final ProductService productService;
+    public final UserService userService;
+    public final PasswordEncoder passwordEncoder;
 
-    public HomePageController(ProductService productService) {
+    public HomePageController(
+            ProductService productService,
+            UserService userService,
+            PasswordEncoder passwordEncoder) {
         this.productService = productService;
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/")
@@ -22,6 +35,30 @@ public class HomePageController {
         List<Product> products = this.productService.fetchProducts();
         model.addAttribute("products", products);
         return "client/homepage/show";
+    }
+
+    @GetMapping("/register")
+    public String getRegisterPage(Model model) {
+        model.addAttribute("registerUser", new RegisterDTO());
+        return "/client/auth/register";
+    }
+
+    @PostMapping("/register")
+    public String handelRegister(Model model, @ModelAttribute("registerUser") RegisterDTO registerDTO) {
+        // Sử dụng mapper tự viết
+        User user = this.userService.registerDTOtoUser(registerDTO);
+
+        String hashPassword = this.passwordEncoder.encode(user.getPassword());
+        user.setPassword(hashPassword);
+        user.setRole(this.userService.getRoleByName("USER"));
+        // save
+        this.userService.handelSaveUser(user);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage(Model model) {
+        return "/client/auth/login";
     }
 
 }
--- src/main/webapp/WEB-INF/view/client/homepage/show.jsp
+++ src/main/webapp/WEB-INF/view/client/homepage/show.jsp
@@ -71,9 +71,6 @@
 
             <jsp:include page="../layout/banner.jsp" />
 
-            <jsp:include page="../layout/feature.jsp" />
-
-
             <!-- Fruits Shop Start-->
             <div class="container-fluid fruite py-5">
                 <div class="container py-5">
@@ -140,7 +137,7 @@
             </div>
             <!-- Fruits Shop End-->
 
-
+            <jsp:include page="../layout/feature.jsp" />
 
             <jsp:include page="../layout/footer.jsp" />
 
